6887b1bc74f7d1c44e66c399ff2fa905f3dbd789
Ali Al Rubh <aalrubh@hotmail.com>
2025-04-16 23:51:00 +0300

    Debugged code

--- build/blackscholes_build/impl/scalar.d
+++ build/blackscholes_build/impl/scalar.d
@@ -1,5 +1,5 @@
-/home/ali/COE403/Assignment/YABMS_AS1/build/blackscholes_build/impl/scalar.o: \
- /home/ali/COE403/Assignment/YABMS_AS1/src/blackscholes/impl/scalar.c \
- /home/ali/COE403/Assignment/YABMS_AS1/src/common/macros.h \
- /home/ali/COE403/Assignment/YABMS_AS1/src/common/types.h \
- /home/ali/COE403/Assignment/YABMS_AS1/src/blackscholes/include/types.h
+/home/ali/COE403/YABMS/build/blackscholes_build/impl/scalar.o: \
+ /home/ali/COE403/YABMS/src/blackscholes/impl/scalar.c \
+ /home/ali/COE403/YABMS/src/common/macros.h \
+ /home/ali/COE403/YABMS/src/common/types.h \
+ /home/ali/COE403/YABMS/src/blackscholes/include/types.h
